f2395859d0c3247ff8c1216b8eb9fbf8f68b4882
githubmhjao <ntumhjao@gmail.com>
2020-11-07 02:43:31 +0800

    Create server.js

---
+++ functions/server.js
@@ -0,0 +1,45 @@
+'use strict'
+
+// courtesy from https://github.com/n0bisuke/netlify-functions-linebot-example
+
+const express = require('express');
+const line = require('@line/bot-sdk');
+const serverless = require('serverless-http'); //追加
+const app = express();
+
+const config = {
+    channelSecret: process.env.lineChannelSecret,
+    channelAccessToken: process.env.lineChannelAccessToken
+};
+
+const router = express.Router();
+router.get('/', (req, res) => res.send('Hello LINE BOT!(GET)'));
+router.post('/webhook', line.middleware(config), (req, res) => {
+    console.log(req.body.events);
+
+    if(req.body.events[0].source.userId: 'Udeadbeefdeadbeefdeadbeefdeadbeef'){
+        res.send('Hello LINE BOT!(POST)');
+        console.log('Webhook Check');
+        return; 
+    }
+
+    Promise
+      .all(req.body.events.map(handleEvent))
+      .then((result) => res.json(result));
+});
+
+const client = new line.Client(config);
+function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+      return Promise.resolve(null);
+    }
+
+    return client.replyMessage(event.replyToken,{
+      type: 'text',
+      text: event.message.text
+    });
+}
+
+app.use('/.netlify/functions/server', router);
+module.exports = app;
+module.exports.handler = serverless(app);
